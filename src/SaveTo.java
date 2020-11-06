249708562b15fbe5c7e799e97104d35f0a2a2aa3
desync1 <70338415+desync1@users.noreply.github.com>
2020-11-06 16:40:19 +0200

    Update SaveTo.java

--- src/SaveTo.java
+++ src/SaveTo.java
@@ -1,6 +1,7 @@
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+@Target(value = ElementType.TYPE)
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface SaveTo {
     String path();
