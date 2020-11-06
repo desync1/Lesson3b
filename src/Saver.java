c89c1f866ae3ec8e3a7ce0e5cc68085a34a20d88
desync1 <70338415+desync1@users.noreply.github.com>
2020-11-06 16:40:43 +0200

    Update Saver.java

--- src/Saver.java
+++ src/Saver.java
@@ -1,6 +1,7 @@
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+@Target(value = ElementType.METHOD)
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface Saver {
 }
